111268509d91a93f96d1dca027346c4cc24fff5d
elena-brd <128292991+elena-brd@users.noreply.github.com>
2023-04-02 18:24:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openSlideMenu() {
+    document.getElementById('slide-navbar').style.width = '300px';
+    document.getElementById('container').style.marginLeft = '300px';
+}
+
+function closeSlideMenu() {
+    document.getElementById('slide-navbar').style.width = '0';
+    document.getElementById('container').style.marginLeft = '0';
+}
